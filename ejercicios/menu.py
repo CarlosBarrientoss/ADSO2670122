0db1fe37a9bbcbfc8025a8b03453692228432ca0
Ivan-Mejia <ivan10villabona@gmail.com>
2023-08-24 20:50:19 -0500

    bootstrap

--- ejercicios/menu.py
+++ ejercicios/menu.py
@@ -1,1 +1,19 @@
-def append():
+positivo=0
+negativo=0
+while True:
+    print ("ingrese un numero: ")
+    print ("el 0 cierra el programa")
+    num =int(input())
+    if num==0:
+        print("cerrando programa")
+        break
+    elif num>0:
+        positivo+=num
+    else :
+        negativo+=1
+    print ("la suma de los positivos: "+str(positivo))
+    print ("la cantidad de los negativos: "+str(negativo))
+        
+        
+    
+    
